ce659753e4041342dd7a27b4fcb1582dbc5cb01a
mithem <mithem@github.com>
2019-09-29 12:59:11 +0200

    messed up

--- config.py
+++
@@ -1,2 +0,0 @@
-class config:
-    helixDir = "/Users/miguel/helix-todos/" #replace this with your favorite dir :)
---
+++ setup.py
@@ -0,0 +1,4 @@
+import os
+from bcolors import col as c
+
+print(c.UNDERLINE + "Do you want to ")
